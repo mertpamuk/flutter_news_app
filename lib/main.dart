22c1ad63e2f309464357a6d2129fd6d2f81c85ae
Mert Pamuk <dev.mertpamuk@gmail.com>
2022-12-17 17:12:00 +0300

    Floating Action Buttons & Pagination added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Material App', home: HomeView());
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomeView(),
+    );
   }
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -11,6 +11,9 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int _page = 1;
+  String _langCode = "";
+  String _lang = "";
   List<NewsModel>? _items;
   bool _isLoading = false;
   late final NewsService _newsService;
@@ -27,46 +30,119 @@ class _HomeViewState extends State<HomeView> {
     });
   }
 
-  Future<void> _fetchItems(String lang) async {
+  Future<void> _fetchItems(String lang, int page) async {
     _changeLoading();
-    _items = await _newsService.fetchNews("$lang");
+    _items = await _newsService.fetchNews(lang, page);
     _changeLoading();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: const Text("News App"),
-      ),
+      appBar: _buildAppBar(),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              itemCount: _items?.length ?? 0,
-              itemBuilder: ((context, index) {
-                return _newsCard(newsModel: _items?[index]);
-              }),
-            ),
+          : _buildBody(),
       drawer: _NewsDrawer(),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () => _fetchItems("")),
+      floatingActionButton: _langCode == "" ? null : _floatingActionButtons(),
+    );
+  }
+
+  ListView _buildBody() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemCount: _items?.length ?? 0,
+      itemBuilder: ((context, index) {
+        return _newsCard(newsModel: _items?[index]);
+      }),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 10,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: _langCode == "" ? null : Text("$_lang | Page: $_page"),
+          ),
+        ),
+      ],
+      title: const Text("News App"),
+      centerTitle: true,
+    );
+  }
+
+  Row _floatingActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            if (_page > 1) {
+              _page--;
+              _fetchItems(_langCode, _page);
+            }
+          },
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          child: const Icon(Icons.arrow_forward_ios),
+          onPressed: () {
+            _page++;
+            _fetchItems(_langCode, _page);
+          },
+        ),
+      ],
     );
   }
 
   Drawer _NewsDrawer() {
     return Drawer(
-        child: ListView.builder(
-      itemCount: Country.countryList.length,
-      itemBuilder: (context, index) => ListTile(
-        title: Text(Country.countryList[index].name),
-        onTap: () {
-          _fetchItems(Country.countryList[index].code);
-          print(Country.countryList[index].code);
-          Navigator.of(context).pop();
-        },
-      ),
+        child: Column(
+      children: [
+        DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.blue, boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+              )
+            ]),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  Text(
+                    "News App",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                  Text(
+                    "Countries:",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ],
+              ),
+            )),
+        Expanded(
+          child: ListView.builder(
+            itemCount: Country.countryList.length,
+            itemBuilder: (context, index) => ListTile(
+              title: Text(Country.countryList[index].name),
+              onTap: () {
+                _langCode = Country.countryList[index].code;
+                _lang = Country.countryList[index].name;
+                _page = 1;
+                _fetchItems(_langCode, _page);
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ),
+      ],
     ));
   }
 }
@@ -92,13 +168,11 @@ class _newsCard extends StatelessWidget {
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
-              child: Image.network(
-                _newsModel?.imageUrl ?? "",
-                errorBuilder: (context, error, stackTrace) {
-                   print(error);
-                   return const Icon(Icons.error);
-                }
-              ),
+              child: Image.network(_newsModel?.imageUrl ?? "",
+                  errorBuilder: (context, error, stackTrace) {
+                print(error);
+                return const Icon(Icons.error);
+              }),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
@@ -108,14 +182,10 @@ class _newsCard extends StatelessWidget {
                     const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
-            Container(
-              child: Text(_newsModel?.description ?? "Unknown Description"),
-            ),
+            Text(_newsModel?.description ?? "Unknown Description"),
           ],
         ),
       ),
     );
   }
 }
-
-//
