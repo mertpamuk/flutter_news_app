8f02f6e11a4f0a003184bfd778638cf3f6adc344
Mert Pamuk <dev.mertpamuk@gmail.com>
2022-12-22 15:29:30 +0300

    Code review & UI Design change

---
+++ lib/constants/categories.dart
@@ -0,0 +1,5 @@
+class NewsCategories {
+  static const List<String> categories = [
+    "general","technology","sports","business","entertainment","health","science"
+  ];
+}
--- lib/service/news_service.dart
+++ lib/service/news_service.dart
@@ -4,17 +4,22 @@ import 'package:dio/dio.dart';
 
 import '../model/news_model.dart';
 
+// NEWS API PAGE https://www.thenewsapi.com
+
 class NewsService {
-  Future<List<NewsModel>?> fetchNews(String lang, int page) async {
+  String apiKey = "Up3ck9GEnYJrqsdNykjm3sv1uSkIfNdIMhEOA5aO";
+
+  Future<List<NewsModel>?> fetchNews(
+      String lang, int page, String category) async {
     final response = await Dio().get(
-        'https://api.thenewsapi.com/v1/news/all?api_token=Up3ck9GEnYJrqsdNykjm3sv1uSkIfNdIMhEOA5aO&language=$lang&page=$page');
+        'https://api.thenewsapi.com/v1/news/all?api_token=$apiKey&language=$lang&page=$page&categories=$category');
 
     if (response.statusCode == HttpStatus.ok) {
       final datas = response.data["data"];
       if (datas is List) {
         return datas.map((e) => NewsModel.fromJson(e)).toList();
       }
-    } else{
+    } else {
       return null;
     }
   }
